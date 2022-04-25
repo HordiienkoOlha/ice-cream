89dca6d922a3148a7f7a279aaa0f06e219dfb947
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-04-25 21:27:07 +0300

    add swiper slider

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+
+new Swiper('.swiper')
+// let swiper = new Swiper('.swiper', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     dynamicBullets: true,
+//   },
+// });
