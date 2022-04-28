effd8f58baf311dfdda5e59b2958c84282732dd9
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-04-28 18:14:43 +0300

    edit slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,10 +1,31 @@
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
 
-new Swiper('.swiper')
-// let swiper = new Swiper('.swiper', {
-//   pagination: {
-//     el: '.swiper-pagination',
-//     dynamicBullets: true,
-//   },
-// });
+// new Swiper('.swiper')
+let swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 120,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+            renderBullet: function (index, className) {
+                return '<span class="'+ className + '">' + (index + 1) + '</span>'
+            }
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+
+
+
+
+    //     spaceBetween: 120,
+    //     pagination: {
+    //       el: ".swiper-pagination",
+    //       clickable: true,
+    //     },
+    //   });
